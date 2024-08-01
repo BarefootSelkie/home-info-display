8161523170d9394158522e7e9bef495c4dcb2c87
Barefoot Selkie <17512788+BarefootSelkie@users.noreply.github.com>
2024-08-01 14:14:45 +0100

    testing text -ry

--- inky-test.py
+++ inky-test.py
@@ -14,6 +14,9 @@ colour = {
     "orange": 6
     }
 
+bigFont = ImageFont.truetype("./LeagueSpartan-Medium.ttf", int(44))
+smallFont = ImageFont.truetype("./LeagueSpartan-Medium.ttf", int(24))
+
 inky = InkyAC073TC1A(resolution=(800, 480))
 display = Image.new(mode="P", size=(480,800), color=(colour["white"]))
 image = ImageDraw.Draw(display)
@@ -21,6 +24,9 @@ image = ImageDraw.Draw(display)
 image.rounded_rectangle([(0,0),(159,119)], radius=6, fill=colour["red"], outline=None, width=1)
 image.rounded_rectangle([(160,0),(319,119)], radius=6, fill=colour["green"], outline=None, width=1)
 image.rounded_rectangle([(320,0),(479,119)], radius=6, fill=colour["blue"], outline=None, width=1)
+
+image.text((80, 60), "Text", colour["black"], font=bigFont, anchor="mm")
+
 
 inky.set_image(display.rotate(90, expand=True))
 inky.show()
