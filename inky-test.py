88c8ca78fa396c515b145ab94f9b0f76637340c5
Barefoot Selkie <17512788+BarefootSelkie@users.noreply.github.com>
2024-08-01 13:06:13 +0100

    trying to create a pallette -ry

--- inky-test.py
+++ inky-test.py
@@ -4,8 +4,18 @@
 from inky.inky_ac073tc1a import Inky as InkyAC073TC1A
 from PIL import Image, ImageDraw, ImageFont
 
+colour = {
+    "black": (28,24,28),
+    "white": (255,255,255),
+    "red": (205,36,37),
+    "blue": (30,29,174),
+    "green": (29,173,35),
+    "yellow": (231,222,35),
+    "orange": (216,123,36)
+    }
+
 inky = InkyAC073TC1A(resolution=(800, 480))
-display = Image.new(mode="RGB", size=(800,480), color=(255,255,255))
+display = Image.new(mode="RGB", size=(800,480), color=(colour["red"]))
 
 inky.set_image(display)
 inky.show()
