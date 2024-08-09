73ec56c8fcd20c74a6616a6b7e3202acfd86e416
Barefoot Selkie <17512788+BarefootSelkie@users.noreply.github.com>
2024-08-09 11:39:59 +0100

    testing date strip -ph

--- inky-test.py
+++ inky-test.py
@@ -21,13 +21,20 @@ inky = InkyAC073TC1A(resolution=(800, 480))
 display = Image.new(mode="P", size=(480,800), color=(colour["white"]))
 image = ImageDraw.Draw(display)
 
-image.rounded_rectangle([(0,0),(157,117)], radius=12, fill=None, outline=colour["black"], width=4)
-image.rounded_rectangle([(161,0),(479,117)], radius=12, fill=None, outline=colour["black"], width=4)
+# Draw the date square in top left of screen
+image.rounded_rectangle([(0,0),(157,157)], radius=12, fill=None, outline=colour["red"], width=4)
+image.rounded_rectangle([(0,0),(157,31)], radius=12, fill=colour["red"], outline=colour["red"], width=4)
+image.text((79,15), "Day", colour["black"], font=smallFont, anchor="mm")
+image.text((79,79), "00", colour["black"], font=bigFont, anchor="mm")
+image.text((79,141), "Mon", colour["black"], font=smallFont, anchor="mm")
+
+# Draw the next trip box next to the current date
+image.rounded_rectangle([(161,0),(479,157)], radius=12, fill=None, outline=colour["black"], width=4)
 
 width = 158
 height = 118
 padding = 3
-offset = 121
+offset = 161
 
 for row in range(3):
     for col in range(2): 
