ad82ebe6196e0931bb2cafbb98eb9b180ae9ca58
Barefoot Selkie <17512788+BarefootSelkie@users.noreply.github.com>
2024-09-30 10:57:14 +0100

    Refactoring -an

--- inky-test.py
+++ inky-test.py
@@ -40,7 +40,20 @@ fontGridLabel = ImageFont.truetype("./ttf/Fredoka-Medium.ttf", int(18))
 fontCalBg = ImageFont.truetype("./ttf/Fredoka-Medium.ttf", int(64))
 fontCalSm = ImageFont.truetype("./ttf/Fredoka-Medium.ttf", int(32))
 
+# Sizing for data grid
+rowWidth = 3
+width = 158
+height = 118
+cellSpacing = 3
+offset = 198
+padding = 10
 
+### Converter functions ###
+
+converters = {
+    "windDirection": lambda degrees: toCompassPoint(float(degrees)),
+    "time": lambda time: time[:5]
+}
 
 ### Functions ###
 
@@ -94,99 +107,50 @@ def wrap(image, text, wrapWidth, font):
 
     return output
 
+def drawCalendar(image):
+    # Draw the calendar square in top left of screen
+    dateNumber = datetime.date.today().strftime('%d')
+    dateDay = datetime.date.today().strftime('%a')
+    dateMonth = datetime.date.today().strftime('%b')
 
-converters = {
-    "windDirection": lambda degrees: toCompassPoint(float(degrees)),
-    "time": lambda time: time[:5]
-}
+    image.rounded_rectangle([(0,0),(157,157)], radius=12, fill=None, outline=colour["red"], width=4)
+    image.rounded_rectangle([(0,0),(157,41)], radius=12, fill=colour["red"], outline=colour["red"], width=4, corners=(True, True, False, False))
+    image.text((79,20), dateMonth, colour["white"], font=fontCalSm, anchor="mm")
+    image.text((79,79), dateNumber, colour["black"], font=fontCalBg, anchor="mm")
+    image.text((79,136), dateDay, colour["black"], font=fontCalSm, anchor="mm")
 
+def drawTrips(image):
+    # Draw the next trip box next to the current date
+    image.rounded_rectangle([(161,0),(479,157)], radius=12, fill=None, outline=colour["black"], width=4)
 
-# Load trips
-try:
-    with open("./trips.yaml", "r") as read_file:
-        trips = yaml.safe_load(read_file)
-except:
-    # logging.critical("Settings file missing")
-    exit()
+    # Get trips that haven't happened yet
+    upcomingTrips = []
+    for trip in trips["trips"]:
+        if datetime.date.fromisoformat(str(trip["date"])) > datetime.date.today():
+            upcomingTrips.append(trip)
 
-# Load in data
-dataSources = {}
+    # Sort the list
+    upcomingTrips.sort(key=lambda trip: trip["date"], reverse=False)
 
-for source in config["sources"]:
-    url = source["url"].format(
-        apikey = source["apikey"], 
-        lat=str(config["location"]["lat"]), 
-        long=str(config["location"]["long"]) 
-    )
+    if len(upcomingTrips) > 0:
+        nextTrip = upcomingTrips[0]
+        nextTripDate = datetime.date.fromisoformat(str(nextTrip["date"]))
+        todayDate = datetime.date.today()
+        daysRemaining = (nextTripDate - todayDate).days
 
-    headers = {}
-    if "headers" in source:
-        for key in source["headers"].keys():
-            headers[key] = source["headers"][key].format(
-                apikey = source["apikey"], 
-                lat=str(config["location"]["lat"]), 
-                long=str(config["location"]["long"]) 
-            )
+        lines = wrap(image, nextTrip["destination"], 294, fontCalSm)
 
-    try:
-        r = requests.get(url, headers=headers)
-        dataSources[source["name"]] = json.loads(r.text)
-    except Exception as e:
-        logging.warning(e)
+        image.text((170,28), lines[0], colour["black"], font=fontCalSm, anchor="lm")
+        if len(lines) > 1:
+            image.text((170,60), lines[1], colour["black"], font=fontCalSm, anchor="lm")
+        image.text((320,108), str(daysRemaining) + " days", colour["black"], font=fontCalBg, anchor="mm")
 
-# Initialise display
-inky = InkyAC073TC1A(resolution=(800, 480))
-display = Image.new(mode="P", size=(480,800), color=(colour["white"]))
-image = ImageDraw.Draw(display)
+def drawMoth(image):
+    # Draw a text box below the date and next trip
+    messageOfHour = "So long and thanks for all the fish"
+    image.rounded_rectangle([(0,161),(479,195)], radius=12, fill=None, outline=colour["blue"], width=4)
+    image.text((239, 161+17), messageOfHour, colour["blue"], font=fontGridLabel, anchor="mm")
 
-# Draw the calendar square in top left of screen
-dateNumber = datetime.date.today().strftime('%d')
-dateDay = datetime.date.today().strftime('%a')
-dateMonth = datetime.date.today().strftime('%b')
-
-image.rounded_rectangle([(0,0),(157,157)], radius=12, fill=None, outline=colour["red"], width=4)
-image.rounded_rectangle([(0,0),(157,41)], radius=12, fill=colour["red"], outline=colour["red"], width=4, corners=(True, True, False, False))
-image.text((79,20), dateMonth, colour["white"], font=fontCalSm, anchor="mm")
-image.text((79,79), dateNumber, colour["black"], font=fontCalBg, anchor="mm")
-image.text((79,136), dateDay, colour["black"], font=fontCalSm, anchor="mm")
-
-# Draw the next trip box next to the current date
-image.rounded_rectangle([(161,0),(479,157)], radius=12, fill=None, outline=colour["black"], width=4)
-
-# Get trips that haven't happened yet
-upcomingTrips = []
-for trip in trips["trips"]:
-    if datetime.date.fromisoformat(str(trip["date"])) > datetime.date.today():
-        upcomingTrips.append(trip)
-
-# Sort the list
-upcomingTrips.sort(key=lambda trip: trip["date"], reverse=False)
-
-if len(upcomingTrips) > 0:
-    nextTrip = upcomingTrips[0]
-    nextTripDate = datetime.date.fromisoformat(str(nextTrip["date"]))
-    todayDate = datetime.date.today()
-    daysRemaining = (nextTripDate - todayDate).days
-
-    lines = wrap(image, nextTrip["destination"], 294, fontCalSm)
-
-    image.text((170,28), lines[0], colour["black"], font=fontCalSm, anchor="lm")
-    if len(lines) > 1:
-        image.text((170,60), lines[1], colour["black"], font=fontCalSm, anchor="lm")
-    image.text((320,108), str(daysRemaining) + " days", colour["black"], font=fontCalBg, anchor="mm")
-
-# Draw a text box below the date and next trip
-messageOfHour = "So long and thanks for all the fish"
-image.rounded_rectangle([(0,161),(479,195)], radius=12, fill=None, outline=colour["blue"], width=4)
-image.text((239, 161+17), messageOfHour, colour["blue"], font=fontGridLabel, anchor="mm")
-
-width = 158
-height = 118
-cellSpacing = 3
-offset = 198
-padding = 10
-
-# look at box design - get template - fill in data - place in image
 
 # edge case boxes
 def boxWeatherIcon(box, position, values):
@@ -249,30 +213,82 @@ def getBoxPosition(index):
     
     return [(stx,sty),(spx,spy)]
 
-index = 0
-rowWidth = 3
-for box in config["boxes"]:
-    position = getBoxPosition(index)    
+def drawDataGrid(image):
+    index = 0
+    for box in config["boxes"]:
+        position = getBoxPosition(index)    
 
-    values = []
-    if "values" in box:
-        for value in box["values"]:
-            values.append(getValue(value))
+        values = []
+        if "values" in box:
+            for value in box["values"]:
+                values.append(getValue(value))
 
-    if "type" in box and box["type"] == "weathericon":
-        boxWeatherIcon(box, position, values)
-    elif box['title'] is not None:
-        if len(values) > 1:
-            boxTitledDual(box, position, values)
+        if "type" in box and box["type"] == "weathericon":
+            boxWeatherIcon(box, position, values)
+        elif box['title'] is not None:
+            if len(values) > 1:
+                boxTitledDual(box, position, values)
+            else:
+                boxTitledBig(box, position, values)
         else:
-            boxTitledBig(box, position, values)
-    else:
-        if len(values) > 1:
-            boxDual(box, position, values)
-        else:
-            boxBig(box, position, values)
-    
-    index = index + 1
+            if len(values) > 1:
+                boxDual(box, position, values)
+            else:
+                boxBig(box, position, values)
+
+        index = index + 1
+
+#### Initialisation
+
+
+# Load trips
+try:
+    with open("./trips.yaml", "r") as read_file:
+        trips = yaml.safe_load(read_file)
+except:
+    # logging.critical("Settings file missing")
+    exit()
+
+# Load in data
+dataSources = {}
+
+for source in config["sources"]:
+    url = source["url"].format(
+        apikey = source["apikey"], 
+        lat=str(config["location"]["lat"]), 
+        long=str(config["location"]["long"]) 
+    )
+
+    headers = {}
+    if "headers" in source:
+        for key in source["headers"].keys():
+            headers[key] = source["headers"][key].format(
+                apikey = source["apikey"], 
+                lat=str(config["location"]["lat"]), 
+                long=str(config["location"]["long"]) 
+            )
+
+    try:
+        r = requests.get(url, headers=headers)
+        dataSources[source["name"]] = json.loads(r.text)
+    except Exception as e:
+        logging.warning(e)
+
+# Initialise display
+inky = InkyAC073TC1A(resolution=(800, 480))
+display = Image.new(mode="P", size=(480,800), color=(colour["white"]))
+image = ImageDraw.Draw(display)
+
+
+### Main code
+
+drawCalendar(image)
+drawTrips(image)
+drawMoth(image)
+drawDataGrid(image)
+
+
+
 
 
 inky.set_image(display.rotate(90, expand=True))
