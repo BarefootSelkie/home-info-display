be2a2d144f1a4715ec8909ad6c16f2299c1b5203
Barefoot Selkie <17512788+BarefootSelkie@users.noreply.github.com>
2024-10-01 14:13:10 +0100

    More debug code -an

--- inky-test.py
+++ inky-test.py
@@ -128,6 +128,7 @@ def requestNextUp():
   global dataNextUp
   try:
     r = requests.get(config["nextup"]["url"], headers={"authorization": "Bearer " + config["nextup"]["apikey"], "content-type": "application/json" })
+    print(r)
     dataNextUp = json.loads(r.text)
     print(r.text)
   except Exception as e:
